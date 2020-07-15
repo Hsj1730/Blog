79455183c4c45619704c5ef4aa00cbaae019435b
Hsj1730 <1830190904@qq.com>
2020-07-16 01:48:28 +0800

    框架搭建-异常处理

---
+++ src/main/java/com/hsjnb/handler/ControllerExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.hsjnb.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author : Joe
+ * @version : 1.0
+ * @date : Created in 2020/07/16 1:01
+ * @description : 错误异常处理
+ */
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    //获取日志对象
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 错误异常处理
+     * @param request 出错的URL
+     * @param e 异常
+     * @return 返回一个错误页面
+     */
+    @ExceptionHandler(Exception.class)      //标识异常的注解
+    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e) throws Exception {
+        logger.error("Request URL : {},Exception : {}",request.getRequestURL(),e);
+
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url",request.getRequestURL());
+        modelAndView.addObject("exception",e);
+        modelAndView.setViewName("error/error");
+        return modelAndView;
+    }
+}
---
+++ src/main/java/com/hsjnb/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.hsjnb.web;
+
+import com.hsjnb.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ *
+ * @author : Joe
+ * @version : 1.0
+ * @date : Created in 2020/07/16 0:38
+ * @description : 返回到首页的控制器
+ */
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+//        int i = 9/0;
+        String blog = null;
+        if (blog == null) {
+            throw new NotFoundException("博客不存在");
+        }
+        return "index";
+    }
+}
