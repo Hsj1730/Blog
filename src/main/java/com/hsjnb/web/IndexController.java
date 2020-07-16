5d6bc88c7fda419ee4118c7f2bec97f08f157e05
Hsj1730 <1830190904@qq.com>
2020-07-16 10:51:45 +0800

    日志处理

--- src/main/java/com/hsjnb/web/IndexController.java
+++ src/main/java/com/hsjnb/web/IndexController.java
@@ -3,6 +3,7 @@ package com.hsjnb.web;
 import com.hsjnb.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
@@ -24,13 +25,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-//        int i = 9/0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
+        System.out.println("---------------index---------------");
         return "index";
     }
 }
