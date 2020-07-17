ffb58e6b12298803650058003241a75fa5f7a31a
Hsj1730 <1830190904@qq.com>
2020-07-17 12:15:50 +0800

    添加前端。完成1/2

--- src/main/java/com/hsjnb/web/IndexController.java
+++ src/main/java/com/hsjnb/web/IndexController.java
@@ -2,7 +2,6 @@ package com.hsjnb.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * █████▒█      ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
