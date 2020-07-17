9908c0cecac5fde421777cffa9be62706d6e5c45
Hsj1730 <1830190904@qq.com>
2020-07-17 22:29:30 +0800

    页面处理

--- src/main/java/com/hsjnb/web/IndexController.java
+++ src/main/java/com/hsjnb/web/IndexController.java
@@ -23,8 +23,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/archives")
+    public String archives() {
+        return "archives";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/friends")
+    public String friends() {
+        return "friends";
+    }
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/message")
+    public String message() {
+        return "message";
+    }
+
+    @GetMapping("/music")
+    public String music() {
+        return "music";
+    }
+
+    @GetMapping("/picture")
+    public String picture() {
+        return "picture";
+    }
+
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+
+    @GetMapping("/tags")
+    public String tags() {
+        return "tags";
+    }
+
+    @GetMapping("/types")
+    public String types() {
+        return "types";
     }
 }
